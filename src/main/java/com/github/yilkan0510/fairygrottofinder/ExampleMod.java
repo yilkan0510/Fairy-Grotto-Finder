c834e86ab63bb86940e50e1620c8dce9415d1564
GitHub Action <action@github.com>
2024-08-28 13:24:06 +0000

    Initialize template repository proper
    Set package name to com.github.yilkan0510.fairygrottofinder
    Set project name to Fairy-Grotto-Finder
    Set modid to fairygrottofinder

--- src/main/java/com/github/yilkan0510/fairygrottofinder/ExampleMod.java
+++ src/main/java/com/github/yilkan0510/fairygrottofinder/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.yilkan0510.fairygrottofinder;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "fairygrottofinder", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/yilkan0510/fairygrottofinder/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/yilkan0510/fairygrottofinder/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.yilkan0510.fairygrottofinder.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
